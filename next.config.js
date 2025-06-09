fee602bba560bc00f132946aac079a823188efc2
Brandon Lee <brandon.lee@greenshield.ca>
2025-06-08 20:19:18 -0400

    Fix syntax error in next.config.js for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
-async headers() {
-  return [
-    {
-      source: "/(.*)",
-      headers: [
-        {
-          key: "Cache-Control",
-          value: "no-cache"
-        },
-      ],
-    },
-  ];
-}, 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache"
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig; 
