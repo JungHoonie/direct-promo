30e54aed04f09f2a8a6eae42493fc2dc848ac633
Brandon Lee <brandon.lee@greenshield.ca>
2025-07-03 14:10:52 -0400

    Fix GTM implementation and Netlify deployment configuration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,14 +34,14 @@ export default function Document() {
         {/* Favicon */}
         <link rel="shortcut icon" type="image/x-icon" href="https://fonts.gstatic.com/s/i/googlematerialicons/more/v6/gm_blue-48dp/1x/gm_more_gm_blue_48dp.png" />
         
-        {/* Google Analytics 4 */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-J2529PY042"></script>
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-J2529PY042');`
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MTRDWHQV');`
           }}
         />
         
