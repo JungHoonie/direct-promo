4f66a2b35b240280e299e459b3629de757fdc2a3
Brandon Lee <brandon.lee@greenshield.ca>
2025-07-03 14:06:16 -0400

    Move GTM script to _app.tsx using Next.js Script component for proper loading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,18 +34,6 @@ export default function Document() {
         {/* Favicon */}
         <link rel="shortcut icon" type="image/x-icon" href="https://fonts.gstatic.com/s/i/googlematerialicons/more/v6/gm_blue-48dp/1x/gm_more_gm_blue_48dp.png" />
         
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MTRDWHQV');`
-          }}
-        />
-
-        
         {/* Google Analytics 4 */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-J2529PY042"></script>
         <script
