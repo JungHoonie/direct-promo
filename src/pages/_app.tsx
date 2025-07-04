87863eb078991436f0bde2772211a97c5259352c
Brandon Lee <brandon.lee@greenshield.ca>
2025-07-04 15:17:01 -0400

    Add dataLayer polyfill and logging for GTM event debugging

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,17 @@ import type { AppProps } from "next/app";
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
+// Polyfill for window.dataLayer
+if (typeof window !== 'undefined') {
+  window.dataLayer = window.dataLayer || [];
+  // Monkey-patch push to log all events
+  const originalPush = window.dataLayer.push;
+  window.dataLayer.push = function() {
+    console.log('dataLayer push:', arguments[0]);
+    return originalPush.apply(this, arguments);
+  };
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
